134a9af6f9ef05ccfa5049a1ede86384eb6e4e55
Mansi Pathak <mshah1221@gmail.com>
2020-08-10 14:08:55 -0400

    add listener for all events

--- index.js
+++ index.js
@@ -2,18 +2,24 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
-module.exports = app => {
+module.exports = (app) => {
   // Your code here
-  app.log('Yay, the app was loaded!')
+  app.log("Yay, the app was loaded!");
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+  app.on("issues.opened", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue!",
+    });
+    return context.github.issues.createComment(issueComment);
+  });
+
+  app.on("*", async (context) => {
+    context.log({ event: context.event, action: context.payload.action });
+  });
 
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-}
+};
