433a21a022e02664b0fc3866b6498bfb25dddab8
Mansi Pathak <mshah1221@gmail.com>
2020-08-13 17:46:23 -0400

    write data to json files and add firebase config

--- index.js
+++ index.js
@@ -5,7 +5,35 @@
 
 const fs = require("fs");
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
+// App's Firebase project configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBUbpjkfcX2DTDVfCugwhK0pyL3msJw_KY",
+  authDomain: "guru-metrics-dashboard.firebaseapp.com",
+  databaseURL: "https://guru-metrics-dashboard.firebaseio.com",
+  projectId: "guru-metrics-dashboard",
+  storageBucket: "guru-metrics-dashboard.appspot.com",
+  messagingSenderId: "513650345220",
+  appId: "1:513650345220:web:758e001112ee5d31263c51",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 module.exports = (app) => {
+  // Get a top level express router to expose new HTTP endpoints
+  const router = app.route("/home");
+
+  // Use express middleware
+  router.use(require("express").static("public"));
+
   app.log("App was loaded!");
 
   app.on("pull_request.merged", async (context) => {
@@ -15,6 +43,12 @@ module.exports = (app) => {
     });
 
     let data = JSON.stringify(context);
+
+    // Send data to /home/pr route
+    router.get("/pr", (req, res) => {
+      res.send(data);
+    });
+
     fs.appendFile("pr-merged-events-data.json", data, function (err) {
       if (err) throw err;
       app.log("pr-merged-events-data.json File Saved!");
@@ -29,6 +63,12 @@ module.exports = (app) => {
     });
 
     let data = JSON.stringify(context);
+
+    // Send data to /home/push route
+    router.get("/push", (req, res) => {
+      res.send(data);
+    });
+
     fs.appendFile("push-events-data.json", data, function (err) {
       if (err) throw err;
       app.log("push-events-data.json File Saved!");
@@ -43,10 +83,23 @@ module.exports = (app) => {
     });
 
     let data = JSON.stringify(context);
-    fs.appendFile("all-events-data.json", data, function (err) {
-      if (err) throw err;
-      app.log("all-events-data.json File Saved!");
+
+    // Send data to /home/all route
+    router.get("/all", (req, res) => {
+      res.send(data);
     });
-    return context.log;
+    if (context.event === "status") {
+      fs.appendFile("status-data.json", data, function (err) {
+        if (err) throw err;
+        app.log("status-data.json File Saved!");
+      });
+      return context.log;
+    } else {
+      fs.appendFile("all-events-data.json", data, function (err) {
+        if (err) throw err;
+        app.log("all-events-data.json File Saved!");
+      });
+      return context.log;
+    }
   });
 };
